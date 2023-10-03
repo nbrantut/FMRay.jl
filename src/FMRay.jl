790dbe821ed173dcd9d22e3ffecff950145c8b2a
Nicolas Brantut <nicolas.brantut@normalesup.org>
2023-10-03 11:56:11 +0100

    add module file

---
+++ src/FMRay.jl
@@ -0,0 +1,21 @@
+module FMRay
+
+using DataStructures
+using Roots #this is for velocity stuff
+import Statistics: middle
+
+export Grid
+export march, traceray
+export refine, refine!, coarsen_derivative, coarsen_derivative!
+export precomputeT, locatelookup
+export domainsize, getcartposition, getcartindex
+export vgroup, vphase, tan_group_angle, phase_angle
+
+include("main.jl")
+include("velocity.jl")
+include("ray.jl")
+include("lookup.jl")
+include("refine.jl")
+include("common.jl")
+
+end
